b8c337a075323ea6cf3a95b22064e27219761a34
Александр <femt0dead3@mail.ru>
2024-07-23 00:14:16 +0300

    task_8++++++

--- src/reducers/productsList.js
+++ src/reducers/productsList.js
@@ -17,20 +17,9 @@ const productsList = (state = initialState, action) => {
             return [state[0], [...state[1], {...state[0][action.payload.id], quantity: 1,}], state[2] + Number(state[0][action.payload.id].price.split('$')[0])]
         case DEL_PRODUCT:
             console.log(state[2] - state[1].filter(el => el.id == Number(action.payload.id))[0].price.split('$')[0]*state[1].filter(el => el.id == Number(action.payload.id))[0].quantity)  
-            //state[2]-Number(state[0][action.payload.id].price.split('$')[0])*
+     
             return [state[0], state[1].filter(el => el.id !== Number(action.payload.id)), state[2] - state[1].filter(el => el.id == Number(action.payload.id))[0].price.split('$')[0]*state[1].filter(el => el.id == Number(action.payload.id))[0].quantity]
-        // case IZ_PRODUCT:
-        //     return state.map(
-        //         ({id , name , description, price, available, completed}) => ({id , name , description, price, available: (id == Number(action.payload.id)+1 ? (available=='Доступен'? 'Не доступно':'Доступен'):available), completed})
-        //     )
-        // case Ed_PRODUCT:
-        //     console.log(action.payload.id);
-        //     return state.map(
-        //         ({id , name , description, price, available, completed}) => ({id , name , description, price, available, completed: (id == Number(action.payload.id)?!completed:completed)}))
-        // case SAVE_PRODUCT:
-        //     console.log(action.payload.el);
-        //     return state.map(
-        //         ({id , name , description, price, available, completed}) => (id == Number(action.payload.el.id)?({id: action.payload.el.id , name: action.payload.el.name , description:action.payload.el.description, price: action.payload.el.price, available:action.payload.el.available, completed}):({id , name , description, price, available, completed})))
+
         case AGAIN_ADD_BASCET_PRODUCT:
             return [state[0], state[1].map(
                 ({id , name , price, size, img,color, quantity}) => ({id, name, price, size, img,color, quantity: ((id == action.payload.id)?quantity+1:quantity)})), state[2] + Number(state[0][action.payload.id].price.split('$')[0])]
